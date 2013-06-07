c3f5c381bfee34002c7d31cfaf103e756ba65a9b
jomo2 <jomo2@linus.netazoic.com>
2013-06-07 15:22:24 -0700

    Initial commit for majd2mm

---
+++ majd2mm.pl
@@ -0,0 +1,172 @@
+#!/usr/bin/perl -w 
+
+# Convert majordomo email commands to mailman email commands
+# Jomo, 6/4/2013
+
+my $email_body="";
+my $cmd;
+my $domainName = "linus.netazoic.com";
+my $line;
+my $from;
+
+my $cmdSub = "http://[domain.name]/mailman/admin/[listname]/members/add?subscribees=[user_email_address]&adminpw=[admin_password]&send_welcome_msg_to_this_batch=0&send_notifications_to_list_owner=0";
+my $cmdUnsub = "http://[domain.name]/mailman/options/[listname]/[user_email_address]?unsub=1&unsubconfirm=1&password=[admin_password]";
+
+my $cmdMM;
+my $logMsg;
+
+
+#************* SET THESE VALUES **************
+my $MM_BIN="/usr/lib/mailman/bin";
+my $MEMBERS_FILE="./members.txt";
+my $LOG_FILE="/var/log/majd2mm.log";
+#********************************************
+
+if(! -d $MM_BIN){ exit("The mailman bin directory not located. Please edit settings.");}
+
+
+sub processCmd($){
+	my($cmd1) = @_;
+	my $ret;
+
+	chomp($cmd1);
+
+
+	if($cmd1 =~ /approve.*/){
+		$cmd1 =~ /approve\s+([\w|\.]+)\s+([\w]+)\s+([\w|_]+)\s+(.*)/;
+		$passwd = $1;
+		$cmdVerb = $2;
+		$list = $3;
+		$email = $4;
+	}
+	elsif($cmd1 =~ /unsubscribe\s+([\w|_|\*]+)\s+(.*)/){
+		#$cmd1 =~ /unsubscribe\s+([\w|_|\*]+)\s+([\w|@|_|\.|\*]+)/;
+		$cmdVerb = "unsubscribe";
+		$list = $1;
+		$email = $2;
+	}
+	else{
+		$cmdVerb = "ERROR";
+		$logMsg = "problem processing: $cmd1";
+	}
+
+
+
+	# Basic authentication is killing the wget idea
+	#if($cmdVerb eq "subscribe"){ 
+	#	$mmEmail = "join-$list";
+	#	$cmdMM = $cmdSub;
+	#}
+	#if($cmdVerb=~/unsub/i){ 
+	#	$mmEmail = 
+	#	$cmdMM = $cmdUnsub;
+	#}
+	#$cmdMM =~s/\[domain.name\]/$domainName/;
+	#$cmdMM =~s/\[listname\]/$list/;
+	#$cmdMM =~s/\[user_email_address\]/$email/;
+	#$cmdMM =~s/\[admin_password\]/$passwd/;
+	#system("wget $cmdMM");
+
+
+
+	# Email options aren't all that great
+	#$mmEmail = $cmdVerb . "_" . $list;
+	#$msg = "subscribe address=$email";	
+	#$from = $majordomoEmail;
+	#send_email($mmEmail,$from,$subject,$msg);
+
+	# Just run commands directly
+
+	&writeMembers($email);
+	if($cmdVerb eq "subscribe"){
+	
+		# just run the add_members command
+		$cmd = "$MM_BIN/add_members -r $MEMBERS_FILE ";
+		$cmd .=" -w y -a n";
+		$cmd .= " $list";
+		$ret = `$cmd`;
+		if(! $?){$logMsg = $list . ": " . $ret}
+		else{	$logMsg = "Error while trying to subscribe $email to $list\n";
+			$logMsg .= "$ret\n";
+			}
+	}
+	elsif($cmdVerb eq "unsubscribe"){
+
+		$email=~s/\*//g;  # remove wildcards in the email address
+		$cmd = "$MM_BIN/remove_members ";
+		if($list eq "*"){ $cmd .= " --fromall";}
+		else{ $cmd .= " $list ";}
+		$cmd .= " $email";
+		$ret = `$cmd`;
+		if(! $?){
+			if($ret){$logMsg = $ret;}
+			#else{$logMsg = "Ran unsubscribe command with no results: $cmd";}
+			else{$logMsg = "Unsubscribed $email from $list";}
+		}
+		else{	$logMsg = "Error while trying to unsubscribe $email from $list\n";
+			$logMsg .= "$ret\n";
+		}
+	   
+	}
+	&writeLog($LOG_FILE, $logMsg);
+
+}
+	
+	
+
+while (<STDIN>){
+
+	if(0){print($_);}
+	$email_body .= $_;
+	$line = $_;
+	if(! $line){ next;}
+	chomp($line);
+	if(! $line){ next;}
+	if ($line=~/^\s*From:.*[\s:\<]\s*([\w\.]+@[\w\.]+)/){
+		$from = $1;next;
+	}
+	if($line=~/approve.*/){
+		$cmd = $line;
+		processCmd($cmd);
+		next;
+	}
+	if($line=~/unsubscribe.*/){
+		$cmd = $line;
+		processCmd($cmd);
+		next;
+	}
+	if($line=~/Subject:.*unsub/){
+		$cmd = "approve ccb.passwd unsubsribe $from *";
+		processCmd($cmd);
+		next;
+	}
+	next;
+	
+}	
+
+sub writeLog{
+# write anything out to a log file
+        ($logfile,$logtext) = @_;
+	chomp($logtext);
+	if($logtext!~/.*\n$/){
+		$logtext .= "\n";
+	}
+        open (FH,">>$logfile");
+        print FH $logtext;
+        close FH;
+        return;
+}
+
+
+sub writeMembers{
+	my $memfile = $MEMBERS_FILE;
+	my ($members) = @_;
+	unlink($memfile);	# zero it out
+	open(FH, ">>$memfile");
+	print(FH $members);
+	close FH;
+	return;
+}
+
+=begin GHOSTCODE
+=end GHOSTCODE
---
+++ mjunsub.pl
@@ -0,0 +1,186 @@
+#!/usr/local/bin/perl 
+
+### jomo 2/2005
+### 8/2008
+### helper for majordomo (?)
+### Look for the original from address in a piece of forwarded e-mail -- unsub that address
+
+### To use, set up procmailrc script in majordomo home directory with following entries:
+### Forward remove request email to majordomo with Subject line of "unsub"
+### mjunsub.pl will send an email to $majordomo with a Subject line of "Unsub Request for $email_address"
+
+##### Unsubscribe requests
+##:0fw
+##* ^Subject:.*(unsubscribe|remove|REMOVE|Unsub Request)
+##| /usr/local/majordomo/wrapper majordomo
+##
+##:0fw
+##* ^Subject: unsub
+##| /usr/local/majordomo/Tools/mjunsub.pl
+
+
+## Config
+my $local_adm	= 'mailadmin@netazoic.com';
+my $logerr	= "/var/log/majordomo.log";
+my $logfile	= "/var/log/majordomo.log";
+my $list	= 'ccb';
+my $majordomo	= 'majordomo@linus.claresco.hq';  
+my $subject 	= "Unsub Request for ";
+my $cmd		= "unsubscribe * ";
+## my $majordomo = 'majordomo@netazoic.com';
+##
+
+
+while(@ARGV){
+	$arg = shift(@ARGV);
+	if( $arg eq '-l'){ $list = shift(@ARGV);}
+	if( $arg eq '-r'){ $flgRcpt = 1;}
+	if( $arg eq '-F'){ $flgLocalDom = 1;}  ## Accept fake local domain
+}
+my $flgFWTxt 	= "-----Original Message-----";
+my $flgFW 	= 0;
+my $flgFrom 	= 0;
+my $txtRcvd 	= "Received:.*$local_dom";
+my $flgAdm	= 1;
+my $flgRcpt	= 0;
+my $email	= '';
+my $debug	= 0;
+
+while (<STDIN>){
+	if(0){print($_);}
+	$email .= $_;
+
+	## Check for end command
+	if(/^end$/){ 
+		## How do we do a last?
+		while(<STDIN>){
+			if(0){print($_);}
+		}
+		next;
+	}
+	## Check for forwarded email or requesting email address
+	if (/^\s*From:.*[\s:\<]\s*([\w\.]+@[\w\.]+)/){
+		if(!$flgFrom){$flgFrom =1;$from1 = $1;next;}
+		else{
+			if(0){ print("From: $1");}
+			$unsub_email = $1;
+			last;
+		}
+	}
+}
+
+chomp($unsub_email);
+## ltrim
+$unsub_email =~ s/^\s+//;
+
+if(! $unsub_email){
+	$msg = "Unsub request does not look like a forwarded email.\n";
+	$msg .= "Cannot process request from $from1\n";
+	log_it($logfile,$msg);
+	exit(1);
+}
+	
+## Process the request
+	$now = localtime;
+	$cmd .= $unsub_email . "\n";
+	$msg = $cmd;
+	$msg .= "end \n";
+	$msg .= ("$now \n");
+	$msg .= ("Unsub request:\n");
+	$msg .= ("From\: $from1\n");
+	$msg .= ("For: $unsub_email\n\n\n");	
+
+	#if($ret){
+	#	$msg .= ("Unsubscribed $unsub_email from $list\n");
+	#	$msg .= ("$ret\n");
+	#}
+	#else{
+	#	$msg .= ("Request not processed.\n");
+	#	if($debug){
+	#		open ERR, ">> $logerr";
+	#		select ERR;
+	#		print "--------------------------------------------\n";
+	#		print "$msg";
+	#		print "--------------------------------------------\n";
+	#		print "$email";
+	#		close ERR;
+	#		select STDOUT;
+	#	}
+	#}
+	if($debug){ print($msg);}
+	$subject .= $unsub_email;
+	send_email($majordomo,$from1,$subject,$msg);
+	#send_email($majordomo,$local_adm,$subject,$msg);
+	log_it($logfile, $subject . "\n");
+	#log_it($logfile,$msg);
+	exit(0);
+	
+##########
+# Log it #
+##########
+sub log_it{
+	my($logfile, $msg) = @_;
+	if($logfile){
+		unless( open LOG, ">>$logfile") {
+			die "Cannot open logfile: $!";
+		}
+		my $ts = Time_Stamp();
+		select LOG;
+		print "$ts: ";
+		print "$msg";
+		select STDOUT;
+		close LOG;
+	}
+	return(0);
+}
+sub Time_Stamp {
+        use vars qw($year $month $day  $hour $min $sec $len $stamp);
+        # use standard function for date/time
+        ($sec, $min, $hour, $day, $month, $year) = localtime();
+        $year = 2000 + $year - 100;
+        $month = $month +1;
+        $len = length($year);
+        if ($len < 2) {$year = "0$year"}
+        $len = length($month);
+        if ($len < 2) {$month = "0$month"}
+        $len = length($day);
+        if ($len < 2) {$day = "0$day"}
+
+        $len = length($sec);
+        if ($len < 2) {$sec = "0$sec"}
+        $len = length($min);
+        if ($len < 2) {$min = "0$min"}
+        $len = length($hour);
+        if ($len < 2) {$hour = "0$hour"}
+        $stamp = "$year$month$day\-$hour" . "$min" . "$sec";
+        return($stamp);
+}
+
+	
+#########
+# Email #
+#########
+sub send_email {
+	my ($to_address, $from_address, $subject, $msg, $log_file, $bcc_address) = @_;
+	my $MAIL_PROGRAM = '/usr/lib/sendmail -oi -t -odq';
+	my (@a);
+	if ($log_file) {
+		open (LOG, "$log_file")  || return(0);
+		@a = <LOG>;
+		close (LOG);
+	}
+
+    open (MAIL, "|$MAIL_PROGRAM")
+		or die "Can't fork for sendmail: $!\n";
+
+	print MAIL "To: $to_address\n";
+	if (defined($bcc_address)) {print MAIL "Bcc: $bcc_address\n";}
+	print MAIL "From: $from_address\n";
+	print MAIL "Subject: $subject\n\n";
+	if ($msg) {print MAIL "$msg";}	
+	if ($log_file) { print MAIL "@a";}
+  	close (MAIL);
+#warn "Sent email: To:  $to_address \nSubject: $subject" ;
+	return (0);
+}
+	
